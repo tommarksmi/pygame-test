7ffeef269957c42c1661bcba745a06997c8dadfb
tommarksmi <thomasmarksmithiii@gmail.com>
2024-08-06 21:45:15 -0400

    the real first commit of pygame leaning

---
+++ entities.py
@@ -0,0 +1,56 @@
+## Enity classes
+import pygame
+from pygame.locals import *
+
+class player(pygame.sprite.Sprite):
+
+    vector = pygame.math.Vector2
+
+    def __init__(self, fric, accel, surface_width, surface_height):
+        super().__init__()
+        self.surf = pygame.Surface((30, 30))
+        self.surf.fill((128, 255, 40))
+        self.rect = self.surf.get_rect(center = (10, 420))
+
+
+        self.pos = self.vector((10, 385))
+        self.vel = self.vector(0, 0)
+        self.acc = self.vector(0, 0)
+        self.fric_const = fric
+        self.accel_const = accel
+        self.parent_width = surface_width
+        self.parent_height = surface_height
+
+    def move(self):
+        self.acc = self.vector(0, 0)
+
+        pressesd_keys = pygame.key.get_pressed()
+
+        if pressesd_keys[K_LEFT]:
+            self.acc.x = -self.accel_const
+        if pressesd_keys[K_RIGHT]:
+            self.acc.x = self.accel_const
+        if pressesd_keys[K_UP]:
+            self.acc.y = -self.accel_const
+        if pressesd_keys[K_DOWN]:
+            self.acc.y = self.accel_const
+
+
+        self.acc.x += self.vel.x * self.fric_const
+        self.acc.y += self.vel.y * self.fric_const
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        if self.pos.x > self.parent_width:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = self.parent_width
+
+        if self.pos.y > self.parent_height:
+            self.pos.y = 0
+        if self.pos.y < 0:
+            self.pos.y = self.parent_height
+
+        self.rect.midbottom = self.pos
+
+
---
+++ pygame-test.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.locals import *
+import time
+import entities
+import sys
+
+pygame.init()
+vector = pygame.math.Vector2
+
+HEIGHT = 450
+WIDTH = 400
+ACCEL = 0.5
+FRICTION = -0.12
+FPS = 60
+
+FramePerSec = pygame.time.Clock()
+
+display_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Game")
+
+player = entities.player(FRICTION, ACCEL, WIDTH, HEIGHT)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    display_surface.fill((0, 0, 0))
+
+    display_surface.blit(player.surf, player.rect)
+
+    pygame.display.update()
+    FramePerSec.tick(FPS)
+    player.move()
