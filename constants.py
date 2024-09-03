d11ac742983a5b11f5acdbaaeb2c772e14bb7450
tommarksmi <thomasmarksmithiii@gmail.com>
2024-09-03 17:34:03 -0400

    Added constants file and contents added game-initaliez file

---
+++ constants.py
@@ -0,0 +1,12 @@
+class Constants():
+    def __init__(self):
+
+        self.HEIGHT = 900
+        self.WIDTH = 800
+        self.ACCEL = 0.5
+        self.FRICTION = -0.12
+        self.FPS = 60
+
+        self.RED = (255, 10, 10)
+        self.YELLOW = (255, 255, 30)
+        self.LIGHT_BLUE = (10, 180, 255)
